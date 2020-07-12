a4781d9cba9117564dcf0ce9eb822604a2a64a6e
linhongcun <9%c!PdgaOU>
2020-07-12 18:15:29 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,42 @@
+<template>
+    <div id="app">
+        <BarChart id="c1" v-bind:option="chartData"></BarChart>
+    </div>
+</template>
+
+<script>
+    import BarChart from "~/components/BarChart";
+
+    export default {
+        components: {
+            BarChart
+        },
+        data() {
+            return {
+                chartData: {
+                    title: {
+                        text: 'ECharts组件化开发'
+                    },
+                    tooltip: {},
+                    legend: {
+                        data: ['销量']
+                    },
+                    xAxis: {
+                        data: ["衬衫", "羊毛衫", "雪纺衫", "裤子", "高跟鞋", "袜子"]
+                    },
+                    yAxis: {},
+                    series: [{
+                        name: '销量',
+                        type: 'bar',
+                        data: [5, 20, 36, 10, 10, 20]
+                    }]
+                }
+            }
+        },
+        created() {
+        },
+        methods: {}
+    }
+</script>
+<style>
+</style>
---
+++ src/components/BarChart.vue
@@ -0,0 +1,35 @@
+<template>
+    <section>
+        <!--注意id动态绑定，并设置宽高-->
+        <div :id="id" style="width:800px; height:500px"></div>
+    </section>
+</template>
+<script>
+    import echarts from 'echarts'
+
+    export default {
+        name: "BarChart",
+        // option 支持动态赋予数据
+        props: ['id', 'option'],
+        data() {
+            return {
+                // 目标 chart，以便后续进行操作
+                chart: null
+            }
+        },
+        created() {
+        },
+        mounted() {
+            // 注意等 echarts的图形容器生成，再对其进行初始化
+            this.initChart();
+        },
+        methods: {
+            initChart() {
+                // 基于准备好的dom，初始化echarts实例，为了通用复用，id必须动态绑定
+                this.chart = echarts.init(document.getElementById(this.id));
+                // 初始化数据
+                this.chart.setOption(this.option)
+            }
+        }
+    }
+</script>
